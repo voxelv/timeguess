ac70cd297d8eafa71f2cf2d4639d24fd10c67e4c
voxelv <timslippy@librem.one>
2021-11-05 13:00:13 -0700

    add other entries

--- timeguess/main.gd
+++ timeguess/main.gd
@@ -10,8 +10,22 @@ const FRI1200_UNIX := 1636138800
 const SUN1200_UNIX := FRI1200_UNIX + 2*24*60*60
 
 const guesses := {
-	"JM": ["Sat", 22, 12, 34, Color.yellow],
+	"JS": ["Fri", 14,26,0, Color.blue],
+	"AA": ["Fri", 16,19,0, Color.pink],
+	"MS": ["Fri", 16,44,0, Color.forestgreen],
+	"EA": ["Fri", 18,35,0, Color.bisque],
+	"B?": ["Fri", 22,10,0, Color.cadetblue],
+	"JM": ["Fri", 22, 12, 34, Color.yellow],
 	"TS": ["Fri", 22,14,0, Color.red],
+	"MP": ["Fri", 22,42,0, Color.purple],
+	"CW": ["Fri", 23,30,0, Color.orange],
+	"PN": ["Fri", 23,44,0, Color.darkmagenta],
+	"LS": ["Sat", 3,56,0, Color.hotpink],
+	"JA": ["Sat", 7,3,0, Color.aqua],
+	"PS": ["Sat", 8,3,0, Color.skyblue],
+	"SW": ["Sat", 11,43,0, Color.fuchsia],
+	"AS": ["Sat", 17,42,0, Color.yellowgreen],
+	"MA": ["Sat", 18,11,0, Color.darkgoldenrod],
 }
 
 func _ready():
@@ -23,7 +37,7 @@ func _ready():
 		
 		labels.add_child(g)
 		g.rect_position.x = ofst
-		ofst += 40
+		ofst += 60
 		g.rect_position.y = labels.rect_size.y * p
 #		var s = "%s: %s" % [guess_name, str(p)]
 		var s = guess_name
